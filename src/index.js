bab7a65965953638b83c6d5a84616411fdc73863
Michael Caceres <63517857+michaeling10@users.noreply.github.com>
2023-12-12 20:53:50 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -182,7 +182,7 @@ function showMovieDetailsInModal(movie) {
         release_date,
         vote_average,
         vote_count,
-        genres_ids,
+        genre_ids,
         popularity,
         original_title,
         poster_path,
