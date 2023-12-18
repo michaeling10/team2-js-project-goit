70a0b3a239c2d75e15e7d08605a3ca5f2f68a636
Michael Caceres <michaeling10@hotmail.com>
2023-12-17 21:56:29 -0500

    Last-Update!

--- src/app/loginCheck.js
+++ src/app/loginCheck.js
@@ -1,5 +1,6 @@
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
+const libraryBtn = document.getElementById('library-btn');
 
 console.log(loggedOutLinks);
 console.log(loggedInLinks);
@@ -8,8 +9,10 @@ export const loginCheck = user => {
   if (user) {
     loggedInLinks.forEach(link => (link.style.display = 'block'));
     loggedOutLinks.forEach(link => (link.style.display = 'none'));
+    libraryBtn.style.display = 'block';
   } else {
     loggedInLinks.forEach(link => (link.style.display = 'none'));
     loggedOutLinks.forEach(link => (link.style.display = 'block'));
+    libraryBtn.style.display = 'none';
   }
 };
